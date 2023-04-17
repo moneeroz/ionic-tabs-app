4df02af0dbb9e317b533a65fb0903652b0a804c4
Moneer <moneer.alromimah@gmail.com>
2023-04-17 17:51:18 -0600

    Ionic Alerts

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,10 +7,34 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent]
+  imports: [IonicModule, ExploreContainerComponent],
 })
 export class Tab2Page {
+  // public alertButtons = ['YES', 'NO'];
 
+  // Customise button action
+  public alertButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      handler: () => {
+        console.log('Alert canceled');
+      },
+    },
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: () => {
+        console.log('Alert confirmed');
+      },
+    },
+    {
+      text: 'Idk',
+      role: 'idk',
+      handler: () => {
+        console.log('I do not know');
+      },
+    },
+  ];
   constructor() {}
-
 }
